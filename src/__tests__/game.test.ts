7d60f74c98abbe75e4b831d37f346ed7afa11a0c
lparolari <luca.parolari23@gmail.com>
2020-11-14 10:56:13 +0100

    feat(game): check board winner

--- src/__tests__/game.test.ts
+++ src/__tests__/game.test.ts
@@ -2,25 +2,24 @@ import { board, checkWinner } from "../game";
 
 describe("game", () => {
   test("controlla array bidimensionale di dimensioni 3x3", () => {
-    expect(board.length).toEqual(3);
-    expect(board[0].length).toEqual(3);
-    expect(board[1].length).toEqual(3);
-    expect(board[2].length).toEqual(3);
+    expect(board.length).toEqual(3 * 3);
   });
 
-  test("verifica allineamenti 1", () => {
-    const board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"];
+  test("verifica vincitore 1", () => {
+    const b1 = ["X", " ", " ", " ", "X", " ", " ", " ", "X"];
+    const b2 = ["X", "O", "O", " ", "X", " ", " ", " ", "X"];
+    const b3 = ["X", "X", "X", "O", "O", "O", " ", " ", " "];
+    const b4 = ["X", "X", " ", " ", " ", " ", " ", "O", "O"];
 
-    expect(checkWinner(board, "X")).toBe(true);
+    expect(checkWinner(b1, "X")).toBe(true);
+    expect(checkWinner(b1, "O")).toBe(false);
+    expect(checkWinner(b1, "Z")).toBe(false);
+    expect(checkWinner(b2, "X")).toBe(true);
+    expect(checkWinner(b2, "O")).toBe(false);
+    expect(checkWinner(b3, "X")).toBe(true);
+    expect(checkWinner(b3, "O")).toBe(true);
+    expect(checkWinner(b4, "X")).toBe(false);
+    expect(checkWinner(b4, "O")).toBe(false);
+    expect.assertions(9);
   });
-
-  //   test("verifica allineamenti 2", () => {
-  //     const board = [
-  //       [" ", "X", " "],
-  //       [" ", "X", " "],
-  //       [" ", " ", "X"],
-  //     ];
-
-  //     expect(checkWinner(board, "X")).toBe(false);
-  //   });
 });
--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,7 @@
-type Board = string[];
+import * as _ from "lodash";
+
+type Entity = string;
+type Board = Entity[];
 
 export const board: Board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
@@ -6,8 +9,29 @@ export function switchPlayer(p: number) {
   return (p + 1) % 2;
 }
 
+export function masks() {
+  return [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ];
+}
+
+export function binarize(board: Board, player: string) {
+  return board.map((x) => (x === player ? 1 : 0));
+}
+
 export function checkWinner(board: Board, player: string) {
-  const b1 = ["X", " ", " ", " ", "X", " ", " ", " ", "X"];
+  const b = binarize(board, player);
+
+  for (const mask of masks()) {
+    if (_.isEqual(b, mask)) return true;
+  }
 
   return false;
 }
